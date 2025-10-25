19d2b3ced68158f5db5afb49d2aaa7ad8af451cb
ComplexorJr <aniketsingh821305@gmail.com>
2025-10-25 16:00:44 +0530

    Add index.html, sw.js, manifest for GitHub Pages root deployment

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "secure-chat-cache-v5";
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(["/", "/index.html"]))
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, resClone)).catch(() => {});
+        return res;
+      });
+    })
+  );
+});
